07f6515a727850a6966a749fe9f4ce1cf4938dfa
Leo Roos <darkgend@gmail.com>
2016-09-22 15:03:06 +0300

    fixed questionnaire.service.ts

--- respondent/src/app/questionnaire/questionnaire.service.ts
+++ respondent/src/app/questionnaire/questionnaire.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
-import { Questionnaire } from './questionnaire';
+import { Questionnaire } from './questionnaire.model';
 
 @Injectable()
 export class QuestionnaireService {
